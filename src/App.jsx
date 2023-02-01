acbe3e0469f8ca53bdf446d87d9a65139dd2f85e
Bekir <bekirkscu@gmail.com>
2023-02-01 14:08:46 +0000

    Created Footer as component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-
+import React from 'react';
+import Footer from './components/Footer';
 
 
 function App() {
     return (
-        <h1>hi</h1>
+    
+        <Footer />
     )
     
 }
