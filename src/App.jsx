ebd13de4116e2d30a5eef8a14831bc58b960d461
hussein bahdon <husseinbahdon1@gmail.com>
2023-02-01 15:07:03 +0000

    Summary ( Updated index, App, Header )

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Footer from './Components/Footer';
 import Main from './Components/main/Main';
 import Header from './Components/header/Header'
-
+import './css/style.css'
 
 function App() {
     return (
