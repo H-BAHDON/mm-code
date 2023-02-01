14570fd76e7c98e53c2cc9151db7d1438fce1867
hussein bahdon <husseinbahdon1@gmail.com>
2023-02-01 14:48:46 +0000

    Updated Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
         <div class="col-md-4 d-flex align-items-center">
           <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
             <svg class="bi" width="30" height="24">
-              <use href="#bootstrap"></use>
+              
             </svg>
           </a>
           <span class="footer-end mb-3 mb-md-0">© 2022 MM-Code</span>
@@ -19,10 +19,10 @@ function Footer() {
                 <use href="#twitter"></use>
               </svg></a></li>
           <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24">
-                <use xlink:href="#instagram"></use>
+               
               </svg></a></li>
           <li class="ms-3"><a class="text-muted" href="#"><svg class="bi" width="24" height="24">
-                <use href="#facebook"></use>
+            
               </svg></a></li>
         </ul>
       </footer>
