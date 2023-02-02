4d67222be42780c4e994d49f9172e228e3c1884c
hussein bahdon <husseinbahdon1@gmail.com>
2023-02-02 13:37:26 +0000

    Created Ace

---
+++ src/Components/main/Ace.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import AceEditor from 'react-ace';
+
+
+
+
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <AceEditor />
+      </div>
+    );
+  }
+}
+
+export default Main;
