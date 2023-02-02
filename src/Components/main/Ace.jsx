b7b6a23dd49a6631997434668660f2b18ef4e878
Bekir <bekirkscu@gmail.com>
2023-02-02 14:40:17 +0000

    Updated AceEditor

--- src/Components/main/Ace.jsx
+++ src/Components/main/Ace.jsx
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 import AceEditor from 'react-ace';
+import Action from './Action';
 
 
 
 
-
-class Main extends Component {
+class Main extends Component { 
   render() {
     return (
       <div>
-        <AceEditor />
+        <AceEditor  placeholder="<h1> BH </h1>" mode="javascript"
+  theme="monokai"  name="blah2"
+  onLoad={Action}
+  // onChange={this.onChange}
+  fontSize={14}
+  showPrintMargin={true}
+  showGutter={true}
+  highlightActiveLine={true}
+  value={`function something(editor) {
+  console.log("Okay");
+}`}
+  setOptions={{
+  showLineNumbers: true,
+  tabSize: 2,
+  }}/>
       </div>
     );
   }
