3adc37db475c426497e1d26c03d688f5bfe76db7
hussein bahdon <husseinbahdon1@gmail.com>
2023-02-02 13:37:16 +0000

    Updated Main

--- src/Components/main/Main.jsx
+++ src/Components/main/Main.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 
 
 
-function Main() {
 
-    
+function Main() {
     return (
         <main>
         <section class="mainPanel">
@@ -14,7 +13,6 @@ function Main() {
             <p>The code below and the information box on the right hand side will always disappear when you're typing your code.
             This will help your visual memory along side the exercise improving your muscle
               memory! It's all for your benefit!</p>
-    
             <button class="nextExercise btn btn-primary" type="button">Next exercise</button>
           </div>
           
