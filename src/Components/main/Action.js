233a61843e22d0539dfc7741d8309d009f9456e4
BEKIRKSU <113244972+BEKIRKSU@users.noreply.github.com>
2023-02-03 19:26:12 +0000

    Update Action

--- src/Components/main/Action.js
+++ src/Components/main/Action.js
@@ -1,13 +1,13 @@
-import React from 'react'
+// import React from 'react'
 
 
-function noCheating() {
-  Main.onCopy = function () { };
-  Main.onCut = function () { };
-  Main.onPaste = function () { };
-  Main.getCopyText = function () { };
-}
+// function noCheating() {
+//   Main.onCopy = function () { };
+//   Main.onCut = function () { };
+//   Main.onPaste = function () { };
+//   Main.getCopyText = function () { };
+// }
 
 
 
-export default noCheating;
+// export default noCheating;
