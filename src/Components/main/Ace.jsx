c9649e24519de94d984cb0d473b0fe135bc8bac8
hussein bahdon <husseinbahdon1@gmail.com>
2023-02-04 16:00:10 +0000

    Updated Ace

--- src/Components/main/Ace.jsx
+++ src/Components/main/Ace.jsx
@@ -9,21 +9,21 @@ class Main extends Component {
   render() {
     return (
       <div>
-        <AceEditor  placeholder="<h1> BH </h1>" mode="javascript"
-  theme="monokai"  name="blah2"
-  onLoad={Action}
-  // onChange={this.onChange}
-  fontSize={14}
-  showPrintMargin={true}
-  showGutter={true}
-  highlightActiveLine={true}
-  value={`function something(editor) {
-  console.log("Okay");
-}`}
-  setOptions={{
-  showLineNumbers: true,
-  tabSize: 2,
-  }}/>
+        <AceEditor  
+            placeholder="<h1> BH </h1>" 
+            mode="javascript"
+            theme="monokai"  name="blah2"
+            onLoad={Action.noCheating}
+            // onChange={this.onChange}
+            fontSize={14}
+            showPrintMargin={true}
+            showGutter={true}
+            highlightActiveLine={true}
+            value={``}
+            setOptions={{
+            showLineNumbers: true,
+            tabSize: 2,
+            }}/>
       </div>
     );
   }
