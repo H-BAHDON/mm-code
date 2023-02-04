a756f810bf65350caad2578ddadf60e3c5157c37
hussein bahdon <husseinbahdon1@gmail.com>
2023-02-04 16:00:19 +0000

    Updated Action

--- src/Components/main/Action.js
+++ src/Components/main/Action.js
@@ -1,30 +1,12 @@
-
+import './Ace'
 class Action {
   constructor() {
     this.noCheating = () => {
-      codeEditor.onCopy = function () { };
-      codeEditor.onCut = function () { };
-      codeEditor.onPaste = function () { };
-      codeEditor.getCopyText = function () { };
+      AceEditor.onCopy = function () { };
+      AceEditor.onCut = function () { };
+      AceEditor.onPaste = function () { };
+      AceEditor.getCopyText = function () { };
     };
-    this.ChooseRandomExercise = (arr) => {
-      RandomExercise = Math.floor(Math.random() * exercisesHtml.length);
-      var item = arr[RandomExercise];
-      return item;
-    };
-
-    // this.nextExercise = () => {
-    //   if (currentExercise <= 3) {
-    //     alert("hello");
-    //   } else
-    //     (
-    //       alert("nopt")
-    //     );
-    //   document.querySelector(".skipMe").addEventListener("click", function () {
-    //     codeEditor.setValue(ChooseRandomExercise(exercisesHtml));
-    //   });
-    // };
-
   }
 }
 
