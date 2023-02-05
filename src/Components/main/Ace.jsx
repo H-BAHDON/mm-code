9ca1c0791cfe596633b2e97978d395e9fb911768
hussein bahdon <husseinbahdon1@gmail.com>
2023-02-05 15:07:03 +0000

    Updated Ace

--- src/Components/main/Ace.jsx
+++ src/Components/main/Ace.jsx
@@ -1,32 +1,38 @@
-import React, { Component } from 'react';
-import AceEditor from 'react-ace';
-import Action from './Action';
+// import React, { Component } from 'react';
+// import AceEditor from 'react-ace';
+// import Action from './Action';
 
 
 
 
-class Main extends Component { 
-  render() {
-    return (
-      <div>
-        <AceEditor  
-            placeholder="<h1> BH </h1>" 
-            mode="javascript"
-            theme="monokai"  name="blah2"
-            onLoad={Action.noCheating}
-            // onChange={this.onChange}
-            fontSize={14}
-            showPrintMargin={true}
-            showGutter={true}
-            highlightActiveLine={true}
-            value={``}
-            setOptions={{
-            showLineNumbers: true,
-            tabSize: 2,
-            }}/>
-      </div>
-    );
-  }
-}
+// class Main extends Component { 
 
-export default Main;
+
+//   render() {
+//     return (
+//       <div>
+//         <AceEditor  
+//             placeholder="<h1> BH </h1>" 
+//             mode="javascript"
+//             theme="monokai"  name="blah2"
+//             onLoad={this.onLoad}
+//             // onChange={this.onChange}
+//             fontSize={14}
+//             showPrintMargin={true}
+//             showGutter={true}
+//             highlightActiveLine={true}
+//             value={``}
+//             onPaste= {(e)=>{
+//               e.preventDefault()
+//               return false;
+//             }}
+//             setOptions={{
+//             showLineNumbers: true,
+//             tabSize: 2,
+//             }}/>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Main;
