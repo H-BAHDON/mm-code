b4cbaf63cc6149748602446c5e4cbf9a60fd24fa
BEKIRKSU <113244972+BEKIRKSU@users.noreply.github.com>
2023-04-24 19:11:22 +0100

    Body updated

--- src/Components/main/Body.jsx
+++ src/Components/main/Body.jsx
@@ -1,45 +1,14 @@
 import React, { Component } from 'react';
-// import AceEditor from 'react-ace';
 import CodeEditor from './CodeEditor';
 
 class Body extends Component {
-  handleOnCopy = (e) => {
-    e.preventDefault();
-  }
-
-  handleOnCut = (e) => {
-    e.preventDefault();
-  }
-
-  handleOnPaste = (e) => {
-    e.preventDefault();
-  }
-
   render() {
     return (
-      <div>
-        {/* <AceEditor
-          placeholder="<h1> BH </h1>"
-          mode="javascript"
-          theme="monokai"
-          name="blah2"
-          fontSize={14}
-          showPrintMargin={true}
-          showGutter={true}
-          highlightActiveLine={true}
-          onCopy={this.handleOnCopy}
-          onCut={this.handleOnCut}
-          onPaste={this.handleOnPaste}
-          value={`function something(editor) {
-            console.log("Okay")
-          }`}
-          setOptions={{
-            showLineNumbers: true,
-            tabSize: 2,
-          }}
-        /> */}
+      <div className="editor-container">
         <CodeEditor />
-      </div>
+          {/* <button className="doneButton btn btn-success" type="button">Begin</button>
+          <button className="checkButton btn btn-success" type="button" hidden="hidden">Check code</button> */}
+        </div>
     );
   }
 }
