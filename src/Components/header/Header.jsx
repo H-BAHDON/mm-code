80823aa9d25bafe0d866cbe01618a8e3f545e2d7
BEKIRKSU <113244972+BEKIRKSU@users.noreply.github.com>
2023-04-24 19:25:52 +0100

    Header updated

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,31 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-
-
-
-
-function Header(){
-    return (
-        <header>
-        <div id="logo">
-          <a href="#">
-            <h1 class="logo-title">MM-Code</h1>
-          </a>
-        </div>
-        <nav class="navbar navbar-expand-lg">
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-              <a class="nav-item nav-link" href="htmlExercise\mainHtml.html">HTML</a>
-              <a class="nav-item nav-link" href="CssExercise\mainCSS.html">CSS</a>
-              <a class="nav-item nav-link" href="#">Bootstrap</a>
-              <a class="nav-item nav-link" href="JavascriptExercise\MainJS.html">Javascript</a>
-            </div>
+function Header() {
+  return (
+    <header>
+      <div id="logo">
+        <a href="#">
+          <h1 className="logo-title">MM-Code</h1>
+        </a>
+      </div>
+      <nav className="navbar navbar-expand-lg">
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-item nav-link" href="htmlExercise/mainHtml.html">HTML</a>
+            <a className="nav-item nav-link" href="CssExercise/mainCSS.html">CSS</a>
+            <a className="nav-item nav-link" href="#">Bootstrap</a>
+            <a className="nav-item nav-link" href="JavascriptExercise/MainJS.html">Javascript</a>
           </div>
-        </nav>
-      </header>
-    
-    )
+        </div>
+      </nav>
+    </header>
+  );
 }
-
 
 export default Header;
