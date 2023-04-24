008f581db54dc92d4ac762107505f2960c028da8
BEKIRKSU <113244972+BEKIRKSU@users.noreply.github.com>
2023-04-24 19:48:59 +0100

    Main updated

--- src/Components/main/Main.jsx
+++ src/Components/main/Main.jsx
@@ -3,19 +3,12 @@ import Body from './Body';
 import CodeEditor from './CodeEditor';
 
 function Main() {
-  const handleOnCopy = (event) => {
-    event.preventDefault();
-  }
-
-  const handleOnPaste = (event) => {
-    event.preventDefault();
-  }
 
   return (
     <main>
       <section className="mainPanel">
         <div className="panel-Description">
-          <h1>HTML Structure</h1>
+          <h1>HTML Memory Building</h1>
           <p>The code below and the information box on the right hand side will always disappear when you're typing your code.
           This will help your visual memory along side the exercise improving your muscle
             memory! It's all for your benefit!</p>
