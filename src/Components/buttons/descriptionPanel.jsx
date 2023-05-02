0067f7aaafa54d35674205c9e925b16eaa2bc5f7
BEKIRKSU <bekirkscu@gmail.com>
2023-05-02 11:43:03 -0700

    HTML

--- src/Components/buttons/descriptionPanel.jsx
+++ src/Components/buttons/descriptionPanel.jsx
@@ -7,9 +7,14 @@ function descriptionPanel() {
       <section className="mainPanel">
         <div className="panel-Description">
           <h1>HTML Muscle Memory</h1>
-          <p>The code below and the information box on the right hand side will always disappear when you're typing your code.
-          This will help your visual memory along side the exercise improving your muscle
-            memory! It's all for your benefit!</p>
+          <p>The code below will disappear as soon as you start typing! If you wish to see the code again click
+            the 'Reset' button below the text editor. If you accurately write out the code without any errors, you can
+            click 'Next Exercise'. If however, the code is too difficult for you to practise with, you can 'Skip' to the next!
+            </p>
+            {/* The wordings could be more playful, but understandable. Especially for the home page. 
+            The more creative and attractive the pages look the more interesting the project will be and the 
+            more time people would spend on the website. E.g. when they hover over the title it pops up saying 
+            'this title was made using HTML and CSS'.*/}
           <button className="nextExercise btn btn-success" type="button">Next exercise</button>
         </div>
       </section>
