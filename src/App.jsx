5f7f258e49b9d876b3c3c26dde9573d526190287
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-05-02 01:54:11 -0700

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
         <>
         <Header />
         <Main />
-        <Footer />
+        <footer />
         </>
     )
     
--- src/Components/main/CodeEditor.jsx
+++ src/Components/main/CodeEditor.jsx
@@ -5,61 +5,31 @@ import 'ace-builds/src-noconflict/theme-dracula';
 import 'ace-builds/src-noconflict/ext-language_tools';
 
 function CodeEditor() {
-  const [content, setContent] = useState('');
-
-  const handleOnChange = (newContent) => {
-    setContent(newContent);
-  };
-
-  const handleOnCopy = (event) => {
-    event.preventDefault();
-  };
-
-  const handleOnCut = (event) => {
-    event.preventDefault();
-  };
-
-  const handleOnPaste = (event) => {
-    event.preventDefault();
-  };
-
-  const handleOnContextMenu = (event) => {
-    event.preventDefault();
-  };
-
-  const handleOnKeyDown = (event) => {
-    // Disable all keyboard shortcuts except Ctrl+A, Ctrl+Z, Ctrl+Y, and Ctrl+X
-    if ((event.ctrlKey || event.metaKey) && !event.shiftKey && !event.altKey) {
-      const keyCode = event.keyCode || event.which || event.charCode;
-      if (keyCode !== 65 && keyCode !== 90 && keyCode !== 89 && keyCode !== 88) {
-        event.preventDefault();
-      }
-    }
-  };
+  
+  let sampleCode = `function Greeting() {
+    console.log("Hello World");
+  };`;
 
   return (
+
     <AceEditor
       mode="javascript"
       theme="dracula"
       fontSize={14}
       width="100%"
       height="100%"
-      value={content}
-      onChange={handleOnChange}
-      showPrintMargin={false}
-      editorProps={{ $blockScrolling: true }}
+      placeholder={sampleCode}
+      showPrintMargin={true}
+      // value = {sampleCode}
+      showGutter={true}
+      editorProps={{ $blockScrolling: false }}
       setOptions={{
         enableBasicAutocompletion: true,
         enableLiveAutocompletion: true,
         enableSnippets: true,
         showLineNumbers: true,
-        tabSize: 2,
+        tabSize: 1,
       }}
-      onCopy={handleOnCopy}
-      onCut={handleOnCut}
-      onPaste={handleOnPaste}
-      onContextMenu={handleOnContextMenu}
-      onKeyDown={handleOnKeyDown}
     />
   );
 }
