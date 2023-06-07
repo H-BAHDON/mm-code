5cbcfbbbbdbe42c8902337c4a27f735b86c8d47b
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-06-06 21:25:13 -0700

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,35 @@
 import React, { useState } from "react";
-import Footer from "./Components/Footer/Footer";
-import Main from "./Components/main/Main";
-import Header from "./Components/header/Header";
+// import Footer from "./Components/Footer/Footer";
+// import Main from "./Components/main/Main";
+// import Header from "./Components/header/Header";
 import "./css/style.css";
+import Login from "./Components/reg&login/Login"
 
 function App() {
-  const [exerciseLanguage, setExerciseLanguage] = useState("html");
+  // const [exerciseLanguage, setExerciseLanguage] = useState("html");
 
-  const handleHTMLClick = () => {
-    setExerciseLanguage("html");
-  };
+  // const handleHTMLClick = () => {
+  //   setExerciseLanguage("html");
+  // };
 
-  const handleJavaScriptClick = () => {
-    setExerciseLanguage("javascript");
-  };
+  // const handleJavaScriptClick = () => {
+  //   setExerciseLanguage("javascript");
+  // };
 
-  const handleCSSClick = () => {
-    setExerciseLanguage("css");
-  };
+  // const handleCSSClick = () => {
+  //   setExerciseLanguage("css");
+  // };
 
   return (
     <>
-      <Header
+      {/* <Header
         handleHTMLClick={handleHTMLClick}
         handleJavaScriptClick={handleJavaScriptClick}
         handleCSSClick={handleCSSClick}
       />
       <Main exerciseLanguage={exerciseLanguage} />
-      <Footer />
+      <Footer /> */}
+      <Login />
     </>
   );
 }
