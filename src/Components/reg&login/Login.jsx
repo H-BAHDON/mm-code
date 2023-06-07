3f4af94a3d03bd97688f747893c23286c52f684e
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-06-06 21:24:43 -0700

    created login page

---
+++ src/Components/reg&login/Login.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import Signup from './Signup';
+import "./login.css"
+
+export default function Login() {
+  const [showSignup, setShowSignup] = useState(false);
+
+  const handleSignup = (formData) => {
+    // Handle signup logic, e.g., send the form data to the server
+    console.log('Signup form submitted:', formData);
+  };
+
+  const handleSignupLinkClick = (e) => {
+    e.preventDefault();
+    setShowSignup(true);
+  };
+
+  const handleSigninClick = (e) => {
+    e.preventDefault();
+    setShowSignup(false);
+  };
+
+  return (
+    <div className="box">
+      <div className="form">
+        <h2>SIGN IN</h2>
+        <div className="inputBox">
+          <input type="text" required />
+          <span>Username</span>
+          <i></i>
+        </div>
+        <div className="inputBox">
+          <input type="password" required />
+          <span>Password</span>
+          <i></i>
+        </div>
+        <div className="links">
+          <a href="#">Forgot password</a>
+          <a href="#" onClick={handleSignupLinkClick}>
+            Sign up
+          </a>
+        </div>
+        <input type="submit" value="Login" />
+      </div>
+
+      {showSignup && (
+        <Signup onSignup={handleSignup} onSigninClick={handleSigninClick} />
+      )}
+    </div>
+  );
+}
