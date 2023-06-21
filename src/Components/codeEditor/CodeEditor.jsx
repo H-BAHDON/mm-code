2a6cb25f7aa80f410098a70fa7c8c2c4e5b661ad
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-06-21 06:39:40 -0700

    CodeEditor clean
    codeEditor now has its on css file

---
+++ src/Components/codeEditor/CodeEditor.jsx
@@ -0,0 +1,96 @@
+import React, { useEffect, useRef } from "react";
+import AceEditor from "react-ace";
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/theme-dracula";
+import "ace-builds/src-noconflict/ext-language_tools";
+import 'ace-builds/src-noconflict/mode-jsx';
+
+import "./codeEditor.css"
+
+function CodeEditor({
+  userCode,
+  setUserCode,
+  initialCode,
+  exerciseLanguage,
+  showModal,
+  nextButton,
+  skipButton,
+  showGuide,
+  checkButton,
+}) {
+  const editorRef = useRef(null);
+
+  useEffect(() => {
+    if (!showModal) {
+      // Focus the editor when the modal is closed
+      editorRef.current?.editor.focus();
+    }
+  }, [showModal, nextButton, skipButton, showGuide, checkButton,exerciseLanguage]);
+
+  useEffect(() => {
+    editorRef.current?.editor.focus();
+  }, []);
+
+   useEffect(() => {
+     if (
+       exerciseLanguage
+     ) {
+       // Clear the editor when exerciseLanguage changes
+       setUserCode("");
+     }
+   // eslint-disable-next-line react-hooks/exhaustive-deps
+   }, [exerciseLanguage]);
+
+  const handleCodeChange = (value) => {
+    if (!showModal && !showGuide) {
+      setUserCode(value);
+    }
+  };
+
+  // useEffect(() => {
+  //   const stop = (e) => {
+  //     e.stopPropagation();
+  //     e.preventDefault();
+  //   };
+
+  //   // const aceEditor = document.querySelector(".ace_editor");
+  //   // aceEditor.addEventListener("paste", stop, true);
+
+  //   return () => {
+  //     aceEditor.removeEventListener("paste", stop, true);
+  //   };
+  // }, []);
+
+  return (
+    <div className="editor">
+    
+      <AceEditor
+        mode={exerciseLanguage === 'react' ? 'javascript' : exerciseLanguage}
+        theme="twilight"
+        fontSize={17}
+        width="100%"
+        placeholder={initialCode}
+        className="placeholder-color"
+        showPrintMargin={false}
+        value={userCode}
+        showGutter={true}
+        editorProps={{ $blockScrolling: false }}
+        setOptions={{
+          enableBasicAutocompletion: true,
+          enableLiveAutocompletion: false,
+          enableSnippets: true,
+          showLineNumbers: true,
+          tabSize: 1,
+          readOnly: showModal || showGuide || checkButton // Make the editor read-only when the modal is open
+        }}
+        onLoad={(editor) => {
+          editor.textInput.getElement().ariaLabel = "editorTextarea";
+        }}
+        onChange={handleCodeChange}
+        ref={editorRef} // Assign the ref to the AceEditor component
+      />
+    </div>
+  );
+}
+
+export default CodeEditor;
