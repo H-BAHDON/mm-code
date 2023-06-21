05e0e53bd49272c65d0c7592df63f6bf814af49c
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-06-21 06:41:14 -0700

    Footer Clean
    footer has now its own css

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,24 +1,37 @@
-// import { getSelectionRange } from '@testing-library/user-event/dist/utils';
+  // import { getSelectionRange } from '@testing-library/user-event/dist/utils';
 import React from 'react';
-
-function Footer() {
+import "./footer.css"
+ 
+  function Footer() {
 
     let thisYear = new Date().getFullYear();
-
+    
     return (
-        <footer>
-            <div className="container footer-content">
-                <footer className="d-flex flex-wrap justify-content-center align-items-center py-3 border-top">
-                    <div className="d-flex align-items-center">
-                        <div className="footer-end mb-3 mb-md-0 mx-auto">© {thisYear}
-                            MM-Code</div>
-                    </div>
-                </footer>
-            </div>
-        </footer>
+      <footer>
+  <div className="container footer-content">
+    <footer className="d-flex flex-wrap justify-content-center align-items-center py-3 border-top">
+      <div className="d-flex align-items-center">
+        {/* <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+          <svg className="bi" width="30" height="24">
+          </svg>
+        </a> */}
+        <div className="footer-end mb-3 mb-md-0 mx-auto">© {thisYear} MM-Code</div>
+      </div>
+      {/* <ul className="nav justify-content-end list-unstyled d-flex">
+        <li className="ms-3"><a className="text-muted" href="#"><svg className="bi" width="24" height="24">
+              <use href="#twitter"></use>
+            </svg></a></li>
+        <li className="ms-3"><a className="text-muted" href="#"><svg className="bi" width="24" height="24">
+            </svg></a></li>
+        <li className="ms-3"><a className="text-muted" href="#"><svg className="bi" width="24" height="24"> 
+            </svg></a></li>
+      </ul> */}
+    </footer>
+  </div>
+</footer>
 
     );
 
-}
+  }
 
-export default Footer;
+  export default Footer;
