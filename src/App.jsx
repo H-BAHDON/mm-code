c34a515679f163843adc3dd08bce1c8f3723cec9
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-07-05 01:20:29 +0100

    Updated App

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Platform from "./Routes/Platform";
-import Home from "./Routes/Home";
+import Platform from "./Routes/Platform/Platform";
+import Home from "./Routes/Home/Home";
+import Login from "./Routes/reg&login/Login"
+import SignUp from "./Routes/reg&login/Signup"
+
 import "./assets/style.css";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+  };
 
   return (
     <>
-   <BrowserRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route
           path="/platform"
-          element={<Platform/>}
+          element={<Platform isLoggedIn={isLoggedIn} />}
         />
+        <Route
+          path="/login"
+          element={<Login onLogin={handleLogin} isLoggedIn={isLoggedIn} />}
+        />
+        <Route
+          path="/signup"
+          element={<SignUp onSignup={handleLogin} />}
+        />
+         {/* <Route path="/userprofile" element={<UserProfile/>}/> */}
       </Routes>
     </BrowserRouter>
     </>
