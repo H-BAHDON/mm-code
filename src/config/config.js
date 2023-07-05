00cd601df45b0b61cf1f6b2bfd26da9296f429ef
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-07-05 01:54:55 +0100

    Updated Config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,5 @@
 // config.js
-const isDevelopment = process.env.NODE_ENV === 'development';
-export const apiUrl = isDevelopment ? process.env.REACT_APP_API_URL : 'http://mmcode.io';
+// const isDevelopment = process.env.NODE_ENV === 'development';
+
+// export const apiUrl = isDevelopment ? process.env.REACT_APP_API_URL : 'http://mmcode.io';
+export const apiUrl = 'http://localhost:3001';
