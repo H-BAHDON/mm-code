950a3de19c1f40c9536ffe10ae07ae3af96c97c3
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-07-05 01:22:02 +0100

    Updated Platform

---
+++ src/Routes/Platform/Platform.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import Header from '../../Components/header/Header';
+import Main from '../../Components/main/Main';
+import Footer from '../../Components/Footer/Footer';
+
+function Platform() {
+
+const [exerciseLanguage, setExerciseLanguage] = useState("html");
+
+  const handleHTMLClick = () => {
+    setExerciseLanguage("html");
+  };
+
+  const handleJavaScriptClick = () => {
+    setExerciseLanguage("javascript");
+  };
+
+  const handleCSSClick = () => {
+    setExerciseLanguage("css");
+  };
+
+  const handleReactClick= () => {
+    setExerciseLanguage("react")
+  }
+
+  const handleSqlClick = () =>{
+    setExerciseLanguage("sql")
+  }
+  const handleTestClick = () =>{
+    setExerciseLanguage("test")
+  }
+
+  return (
+   <>
+    <Header
+        handleHTMLClick={handleHTMLClick}
+        handleJavaScriptClick={handleJavaScriptClick}
+        handleCSSClick={handleCSSClick}
+        handleReactClick={handleReactClick}
+        handleSqlClick={handleSqlClick}
+        handleTestClick={handleTestClick}
+      />
+      <Main exerciseLanguage={exerciseLanguage} />
+      <Footer />
+
+   </>
+  )
+}
+
+export default Platform
