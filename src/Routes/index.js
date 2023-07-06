da5d74de792f989282cc0af1599eed0b0394fd43
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-07-06 16:54:06 +0100

    Created index.js
    created index for my routes

---
+++ src/Routes/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Home from './Home/Home';
+import Platform from './Platform/Platform';
+import Login from './reg&login/Login';
+import Signup from './reg&login/Signup';
+import UserProfile from './User-Dashboard/userProfile';
+import { getSessionData } from "../Helpers/session";
+import { Navigate } from 'react-router-dom';
+
+const PrivateRoute = ({ element, path }) => {
+  const sessionData = getSessionData(); // Retrieve the session data
+  const isLoggedIn = sessionData != null; // Check if user is logged in
+
+  return isLoggedIn ? element : <Navigate to="/login" />;
+};
+
+export const routes = [
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/platform',
+    element: <PrivateRoute element={<Platform />} />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
+  },
+];
