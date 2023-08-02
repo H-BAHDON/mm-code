bc9cf8ff47f5044e6773152e571baac89f553a87
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-08-02 15:28:52 +0100

    updated homepage

--- src/Routes/Home/Home.jsx
+++ src/Routes/Home/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Header from "../../Components/header/LogoBar"
 
 function Home() {
-  return (
+  return (<>
+    <Header/>
     <div>Home</div>
+    </>
   )
 }
 
