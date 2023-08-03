803eb5a0be48fe415089b46eaeb1f176419b3e05
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-08-03 14:15:09 +0100

    Updated config

--- src/config/config.js
+++ src/config/config.js
@@ -2,4 +2,4 @@
 // const isDevelopment = process.env.NODE_ENV === 'development';
 
 // export const apiUrl = isDevelopment ? process.env.REACT_APP_API_URL : 'http://mmcode.io';
-export const apiUrl = 'https://mm-code-api-b4f2aff44087.herokuapp.com/';
+export const apiUrl = 'https://mm-code-api-b4f2aff44087.herokuapp.com';
