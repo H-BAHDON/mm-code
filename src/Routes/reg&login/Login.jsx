3df3ca87a1b7210f0fbcd09e0241be610fc87664
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-09-26 16:23:58 +0100

    .

--- src/Routes/reg&login/Login.jsx
+++ src/Routes/reg&login/Login.jsx
@@ -1,44 +1,21 @@
-import React from 'react';
-import Grid from '@mui/material/Grid';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import React, { useState, useEffect } from 'react';
+
+import "./login.css";
 import Header from '../../Components/header/Header';
+
 import { apiUrl } from '../../config/config';
-import './login.css'; // Import the CSS file
 
 export default function Login() {
+
   return (
     <>
-      <Header showNavigation={false} />
+      <Header showNavigation={false}/>
 
-      <Grid
-        container
-        className="center-container margin-top margin-bottom" 
-      >
-        <Grid item xs={12}>
-          <Typography variant="h4" gutterBottom>
-            Login with Social Media
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <Button
-            variant="contained"
-            color="primary"
-            href={`${apiUrl}/auth/google`}
-          >
-            Login with Google
-          </Button>
-        </Grid>
-        <Grid item xs={12}>
-          <Button
-            variant="contained"
-            color="secondary"
-            href={`${apiUrl}/auth/github`}
-          >
-            Login with GitHub
-          </Button>
-        </Grid>
-      </Grid>
+      <div className="login-container">
+        <h1>Login with Social Media</h1>
+        <a href={`${apiUrl}/auth/google`} className="btn-google">Login with Google</a>
+        <a href={`${apiUrl}/auth/github`} className="btn-github">Login with GitHub</a>
+      </div>
     </>
   );
 }
