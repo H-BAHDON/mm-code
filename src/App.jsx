ef6e1de7271a7f50bd9c417953450625f40e6866
BEKIRKSU <bekirkscu@gmail.com>
2023-10-09 18:34:29 +0100

    Demo button made.

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,15 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { routes } from './Routes/index';
 import { getSessionData } from "./Helpers/authHelpers";
 import Footer from './Components/Footer/Footer';
+import DemoPlatform from './Routes/DemoPlatform/DemoPlatform'
 import "./app.css";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
-    // Check if a session exists (e.g., by checking if the session data exists in local storage or cookies)
-    const sessionData = getSessionData(); // Implement this function to retrieve session data
+    
+    const sessionData = getSessionData(); 
 
     if (sessionData) {
       setIsLoggedIn(true);
@@ -18,7 +19,7 @@ function App() {
   }, []);
 
   const handleLogin = (sessionData) => {
-    // Perform login logic here and set the session data
+
     setIsLoggedIn(true);
   };
 
@@ -26,15 +27,16 @@ function App() {
   return (
     <>
    <BrowserRouter>
-      <Routes>
-        {routes.map((route, index) => (
-          <Route
-            key={index}
-            path={route.path}
-            element={React.cloneElement(route.element, { onLogin: handleLogin })}
-          />
-        ))}
-      </Routes>
+   <Routes>
+  {routes.map((route, index) => (
+    <Route
+      key={index}
+      path={route.path}
+      element={React.cloneElement(route.element, { onLogin: handleLogin })}
+    />
+  ))}
+  <Route path="/demo" element={<DemoPlatform />} />
+</Routes>
       <Footer />
     </BrowserRouter>
     </>
--- src/Routes/Home/Home.jsx
+++ src/Routes/Home/Home.jsx
@@ -5,19 +5,29 @@ import "./Home.css";
 function Home() {
   const navigate = useNavigate();
   const handleGetStartedClick = () => {
-    // Replace '/platform' with the actual path of your desired page
+    
     navigate('/platform');
   };
+
+ const handleDemoClick = () => {
+    navigate('/demo');
+};
 
   return (
     <>
       <div className="container">
-        <div className="d-flex align-items-center justify-content-center flex-column min-vh-100 content-container">
-          <h1 className="display-3 mb-4">MM-code</h1>
-          <h2 className="lead mb-4">Let's Get Started</h2>
+          <h1 className="display-3 mb-4">MM-Code</h1>
+          <h2 className="lead mb-4">with its active real-time users, emphasizes repetition and targeted focus, drawing inspiration from
+the philosophy of deep practice and the path to expertise. Our aim is to nurture intuitive coding abilities and tackle the initial learning
+hurdles we recognized early in the programming journey.</h2>
+<div className="d-flex align-items-center justify-content-center flex-column min-vh-100 content-container"></div>
+<div className="button-group">
           <button onClick={handleGetStartedClick} className="btn btn-success btn-lg">
             Get Started
             <i className="ml-2 fas fa-arrow-right"></i>
+          </button>
+          <button onClick={handleDemoClick} className="btn btn-info btn-lg">
+            Demo
           </button>
         </div>
       </div>
