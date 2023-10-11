a59ab22d48bea3e35658441ac15b93d6a4852805
BEKIRKSU <bekirkscu@gmail.com>
2023-10-11 21:10:31 +0100

    Buttons made

--- src/Routes/Home/Home.jsx
+++ src/Routes/Home/Home.jsx
@@ -21,36 +21,15 @@ function Home() {
                 <p>Embark on a journey of deep practice with us, overcoming initial challenges to attain coding expertise.</p>
                 </div>
                <div className="button-group">
-          <button onClick={handleGetStartedClick} className="btn btn-success btn-lg">
-            Get Started
+          <button onClick={handleGetStartedClick} className="journey-on-button">
+            Journey On
           </button>
-          <button onClick={handleDemoClick} className="btn btn-info btn-lg">Demo</button>
+          <button onClick={handleDemoClick} className="demo-button">Try the Demo</button>
         </div>
             </main>
         </div>
     );
 }
-
-//   return (
-//     <>
-//       <div className="container">
-//           <h1 className="title">MM-Code</h1>
-//           <h2 className="intro">Leveraging the power of real-time user activity, we embrace the philosophy of deep practice and the journey towards expertise.
-//            Our mission is to cultivate intuitive coding abilities, providing solutions to the initial challenges 
-//            faced at the start of one's programming journey.</h2>
-// {/* <div className="d-flex align-items-center justify-content-center flex-column min-vh-100 content-container"></div> */}
-// <div className="button-group">
-//           <button onClick={handleGetStartedClick} className="btn btn-success btn-lg">
-//             Get Started
-//             <i className="ml-2 fas fa-arrow-right"></i>
-//           </button>
-//           <button onClick={handleDemoClick} className="btn btn-info btn-lg">
-//             Demo
-//           </button>
-//         </div>
-//       </div>
-//     </>
-//   );
 
 
 export default Home;
