a920a1a5b7410f8221b87e7c8909be0a7c9f022a
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-10-12 12:27:26 +0100

    fix the footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -7,52 +7,19 @@ function Footer() {
   const thisYear = new Date().getFullYear();
 
   return (
-    <Grid container >
-      <Grid item xs={11}>
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-md-9 text-center">
-              <div className="social mb-4">
-                {/* <h3>MM-Code</h3> */}
-                <ul className="list-unstyled">
-                  <li className="in">
-                    <a href="#">
-                      <span className="icon-instagram"></span>
-                    </a>
-                  </li>
-                  <li className="fb">
-                    <a href="#">
-                      <span className="icon-facebook"></span>
-                    </a>
-                  </li>
-                  <li className="tw">
-                    <a href="#">
-                      <span className="icon-twitter"></span>
-                    </a>
-                  </li>
-                  <li className="pin">
-                    <a href="#">
-                      <span className="icon-pinterest"></span>
-                    </a>
-                  </li>
-                  <li className="dr">
-                    <a href="#">
-                      <span className="icon-dribbble"></span>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-
+    
+        <div className="Footer-container">
+          <div>
+            <div>
               <div className="copyright">
-                <p className="mb-0">
+                <p>
                   <small>&copy; MMCode™ All Rights Reserved. {thisYear}</small>
                 </p>
               </div>
             </div>
           </div>
         </div>
-      </Grid>
-    </Grid>
+  
   );
 }
 
