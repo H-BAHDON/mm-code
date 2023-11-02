18230230ce6da28f3825a286ff2e89fd1eab1e47
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-11-02 15:52:09 +0000

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { routes } from './Routes/index';
-import { getSessionData } from "./Helpers/authHelpers";
 import Footer from './Components/Footer/Footer';
-import DemoPlatform from './Routes/DemoPlatform/DemoPlatform'
-import "./app.css";
+import './app.css';
+import { AuthProvider, useAuth } from './hooks/useAuth';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    
-    const sessionData = getSessionData(); 
-
-    if (sessionData) {
-      setIsLoggedIn(true);
-    }
-  }, []);
-
-  const handleLogin = (sessionData) => {
-
-    setIsLoggedIn(true);
-  };
 
 
   return (
-    <>
-   <BrowserRouter>
-   <Routes>
-  {routes.map((route, index) => (
-    <Route
-      key={index}
-      path={route.path}
-      element={React.cloneElement(route.element, { onLogin: handleLogin })}
-    />
-  ))}
-</Routes>
-      <Footer />
-    </BrowserRouter>
-    </>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          {routes.map((route, index) => (
+            <Route key={index} path={route.path} element={route.element} />
+          ))}
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </AuthProvider>
   );
 }
+
+
 
 export default App;
