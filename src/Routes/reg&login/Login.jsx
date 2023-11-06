bd8bc1f68467b04394ec9d77dc2b41dc4e47b1e8
Hussein Bahdon <35459369+H-BAHDON@users.noreply.github.com>
2023-11-06 15:20:49 +0000

    Updated Login

--- src/Routes/reg&login/Login.jsx
+++ src/Routes/reg&login/Login.jsx
@@ -1,21 +1,31 @@
-import React, { useState, useEffect } from 'react';
-
-import "./login.css";
-import Header from '../../Components/header/Header';
+import React from 'react';
+import { Button, Box, Typography, Container } from '@mui/material';
 import { apiUrl } from '../../config/env_config';
+import Header from '../../Components/header/Header';
+import "./login.css";
+
 export default function Login() {
-
-  
-
   return (
     <>
+      <div className="parent-container">
+        <div className="square red top-right hu__hu_r_"></div>
+        <div className="square blue-dark left-bottom hu_hu_animation"></div>
+      </div>
       <Header showNavigation={false}/>
 
-      <div className="login-container">
-        <h1>Login with Social Media</h1>
-        <a href={`${apiUrl}/auth/google`} className="btn-google">Login with Google</a>
-        <a href={`${apiUrl}/auth/github`} className="btn-github">Login with GitHub</a>
-      </div>
+      <Container maxWidth="sm" className="login-container">
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 2 }}>
+          <Typography variant="h3" align="center" gutterBottom>
+            Login
+          </Typography>
+          <Typography variant="subtitle1" align="center" gutterBottom>
+            "Where Creativity Meets MMCode, Your Journey Begins."
+          </Typography>
+          <Button href={`${apiUrl}/auth/google`} variant="contained" color="primary" className="btn-google">
+            ...with Google
+          </Button>
+        </Box>
+      </Container>
     </>
   );
 }
