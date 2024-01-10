875d28da80c67d79b39c2fe61346224f82b22380
Hussein Bahdon <husseinbahdon1@gmail.com>
2024-01-10 00:55:12 +0000

    "updated env files"

--- src/config/env_config.js
+++ src/config/env_config.js
@@ -2,19 +2,8 @@
 // const isDevelopment = process.env.NODE_ENV === 'development';
 
 // export const apiUrl = isDevelopment ? process.env.REACT_APP_API_URL : 'http://mmcode.io';
-// export const apiUrl = 'http://localhost:3001';
+export const apiUrl = 'http://localhost:3001';
 
-export const apiUrl = 'https://mm-code-api-b4f2aff44087.herokuapp.com';
+// export const apiUrl = 'https://mm-code-api-b4f2aff44087.herokuapp.com';
 
-
-
-// const prod_config = {../../config/config
-//     env: "prod",
-//     apiGateway: {
-//       URL: "http://localhost:3001",
-//     },
-   
-//   };
-  
-//   export default prod_config;
   
